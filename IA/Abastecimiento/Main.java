b4a565b34cd8f76e4132b2d43b6e0e39b166890b
carlacampas <ccg.campas@gmail.com>
2021-10-12 20:15:11 +0200

    iniciar main

--- IA/Abastecimiento/Main.java
+++ IA/Abastecimiento/Main.java
@@ -1,8 +1,31 @@
 package Abastecimiento;
 
+import java.util.Scanner;
+
+import IA.Gasolina.CentrosDistribucion;
+import IA.Gasolina.Gasolineras;
+
 public class Main {
     public static void main(String[] args) {
-        // nou estat
+    	Scanner sc = new Scanner(System.in);
+    	int ngas, seedGas;
+    	
+    	System.out.println ("Introduce el numero de gasolineras: ");
+    	ngas = sc.nextInt();
+    	System.out.println ("Introduce la semilla para el generador de numeros aleatorios de gasolineras: ");
+    	seedGas = sc.nextInt();
+    	Gasolineras gasolineras = new Gasolineras (ngas, seedGas);
+    	
+    	int ncen, mult, seedCen;
+    	System.out.println ("Introduce el numero de centros de distribucion: ");
+    	ncen = sc.nextInt();
+    	System.out.println ("Introduce la multiplicidad en la misma posicion: ");
+    	mult = sc.nextInt();
+    	System.out.println ("Introduce la semilla para el generador de numeros aleatorios de centros de distribucion: ");
+    	seedCen = sc.nextInt();
+    	CentrosDistribucion centrosDistrbucion = new CentrosDistribucion (ncen, mult, seedCen);
+  
+    	AbastecimientoState as = new AbastecimientoState (gasolineras, centrosDistrbucion);
         // genera solucio
         // imprimir estat
     }
