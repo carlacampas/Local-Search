5547427966a601fe9be3664b1295f246da10a429
laia.valles <laia.valles@upc.edu>
2021-10-13 14:35:35 +0200

    Fix heuristics bugs (Now test is passing)

--- IA/Abastecimiento/AbastecimientoHeuristicFunction1.java
+++ IA/Abastecimiento/AbastecimientoHeuristicFunction1.java
@@ -4,8 +4,6 @@ package Abastecimiento;
 import java.util.*;
 
 import aima.search.framework.HeuristicFunction;
-
-import IA.Gasolina.Distribucion;
 import IA.Gasolina.Gasolinera;
 
 // Aquest valora els costos.
@@ -18,46 +16,30 @@ public class AbastecimientoHeuristicFunction1 implements HeuristicFunction {
     	
     	AbastecimientoState estado = (AbastecimientoState) state;
 
-        double precioTotal = 0.0;
-        double distanciaTotal = 0.0;
+        double precioEnDepositos = 0.0;
+        ArrayList<Integer> distancias = estado.getDistancias();
 
         // Recorrido por las asignaciones para calcular la suma del precio
         // de las peticiones atendidas y la distancia total recorrida
         for (ArrayList<Peticion> listaPeticiones : estado.getAsignaciones()) {
-            double distanciaCamion = 0.0;
-            Gasolinera gasolineraAnterior = null;
             for (int i = 0; i < listaPeticiones.size(); i++) {
 
                 Peticion peticion = listaPeticiones.get(i);
                 Gasolinera gasolinera = estado.gasolineras.get(peticion.get().a);
-                Distribucion distribucion = estado.centrosDistribucion.get(i);
 
-                // Añadir distancia recorrida por una petición a la distancia recorrida por UN camión:
-                if (i == 0) { // Primer pedido: Distancia entre el centro de distribución y la gasolinera que ha hecho el pedido
-                    distanciaCamion += calcularDistancias(distribucion.getCoordX(), distribucion.getCoordY(), gasolinera.getCoordX(), gasolinera.getCoordY());
-                }
-                else { // Distancia entre la gasolinera que ha hecho el pedido anterior y la gasolinera del pedido actual
-                    distanciaCamion += calcularDistancias(gasolineraAnterior.getCoordX(), gasolineraAnterior.getCoordY(), gasolinera.getCoordX(), gasolinera.getCoordY());
-                    if (i == listaPeticiones.size()) { // Último pedido: Además también se sumala distancia entre la gasolinera y el centro de distribucion
-                        distanciaCamion += calcularDistancias(gasolinera.getCoordX(), gasolinera.getCoordY(), distribucion.getCoordX(), distribucion.getCoordY());
-                    }
-                }
-                gasolineraAnterior = gasolinera;
-
-                // Añadir precio de una peticion al precio total:
+                // A�adir precio de una peticion al precio total:
                 int diasPendientes = gasolinera.getPeticiones().get(peticion.get().b);
-                if (diasPendientes == 0) precioTotal += VALOR_DEPOSITO * 1.02;
-                else precioTotal += VALOR_DEPOSITO * ((100 - 2 ^ diasPendientes) / 100);
+                if (diasPendientes == 0) precioEnDepositos += 1.02;
+                else precioEnDepositos += (100 - Math.pow(2, diasPendientes)) / 100;
             }
-            distanciaTotal += distanciaCamion;
         }
         
-        // TODO: Erase prints
-        System.out.println("Real");
-        System.out.println(precioTotal);
-        System.out.println(distanciaTotal);
+        double kilometros = 0;
+        for (Integer distancia : distancias) {
+        	kilometros += AbastecimientoState.maxDist - distancia;
+        }
         
-        return precioTotal - COSTE_KILOMETRO * distanciaTotal;
+        return VALOR_DEPOSITO * precioEnDepositos - COSTE_KILOMETRO * kilometros;
     }
 
     static int calcularDistancias(int fromX, int fromY, int toX, int toY) {
